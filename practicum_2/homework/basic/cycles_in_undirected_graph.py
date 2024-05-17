0857fde1a766a4bd825e245412e79eb52aa826c9
Sergeeva Aleksandra <sergeevaaleksandra743@gmail.com>
2024-05-17 17:40:51 +0300

    practicum 2

--- practicum_2/homework/basic/cycles_in_undirected_graph.py
+++ practicum_2/homework/basic/cycles_in_undirected_graph.py
@@ -8,33 +8,34 @@ TEST_GRAPH_FILES = [
 ]
 
 
-def has_cycles(g: nx.Graph, root, parents:list[int], status:list[int]):
-    not_visited = 0; visited = 1; double_visited = -1;
-    status[int(root)] = visited
-    answer = True
-    while answer == True:
-        for i in g.neighbors(root):
-            if status[int(i)] == not_visited :
-                 parents[int(i)] = root #чтобы не попасть туда же
-                 for j in g.neighbors(i):
-                    if ( j != root[int(i) & status[int(j)] == visited]):
-                        answer = False
-    return answer
+def has_cycles(G: nx.Graph):
+    global flag
+    double_visited = -1
+    visited = 1
+    not_visited = 0
+    status = [not_visited] * len(G)
+    parent = [None] * len(G)
+    def dfs_visit(node):
+        global flag
+        status[int(node)] = visited
+        for i in G.neighbors(node):
+            if status[int(i)] == not_visited:
+                parent[int(i)] = node
+                for j in G.neighbors(i):
+                    if (status[int(j)] == visited or status[int(j)] == double_visited) and (j != parent[int(i)]):
+                        flag = True
+                dfs_visit(i)
+        status[int(node)] = double_visited
 
+    flag = False
+    for node in G:
+        if status[int(node)] == not_visited:
+            dfs_visit(node)
+    return flag
 
 if __name__ == "__main__":
     for filename in TEST_GRAPH_FILES:
         #G = nx.read_edgelist(f"D://algorithm//spbu-fundamentals-of-algorithms//practicum_2//homework//basic//{filename}", create_using=nx.Graph)
         # Output whether it has cycles
         G = nx.read_edgelist(f"C://Users/Alexandra//Documents//Универ//Algos//fundamentals-of-algorithms//practicum_2//homework//basic//{filename}", create_using=nx.Graph)
-        answer = True
-        for i in G:
-            status = [0] * len(G)
-            parents = [0] * len(G)
-            while answer != False:
-                if has_cycles(G, i, status, parents) == False:
-                    answer = False
-    if answer == False:
-            print(f"Graph {filename} is cycled")
-    else:
-            print(f"Graph {filename} is acycled")
+        print(f"Graph {filename} has cycles: {has_cycles(G)}")
