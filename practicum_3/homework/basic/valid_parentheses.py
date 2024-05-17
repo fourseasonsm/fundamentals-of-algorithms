fcac785091a0a7949446e5c750ea27a2067e03b3
Sergeeva Aleksandra <sergeevaaleksandra743@gmail.com>
2024-05-17 17:57:07 +0300

    hw3 with signature

--- practicum_3/homework/basic/valid_parentheses.py
+++ practicum_3/homework/basic/valid_parentheses.py
@@ -3,39 +3,28 @@ from typing import Any
 import yaml
 import numpy as np
 from numpy.typing import NDArray
-
-from src.common import ProblemCase
+#from src.common import ProblemCase
 
 
 class Stack:
-    """LIFO queue"""
-
     def __init__(self, max_n: int, dtype: Any) -> None:
         self._array: NDArray = np.zeros((max_n,), dtype=dtype)  # internal array
         self._top_i: int = -1  # index of the most recently inserted element
 
     def empty(self) -> bool:
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
+        return self._top_i == -1
 
     def push(self, x: Any) -> None:
-        """Complexity: O(1)"""
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
+        self._array[self._top_i + 1] = x
+        self._top_i += 1
 
-        pass
 
     def pop(self) -> Any:
-        """Complexity: O(1)"""
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
+        x = self._array[self._top_i]
+        self._array[self._top_i] = 0
+        self._top_i -= 1
+        return x
 
-        pass
 
 
 class StackUnderflowException(BaseException):
@@ -58,18 +47,24 @@ def get_starting_symbol(sym: str) -> str:
 
 
 def are_parentheses_valid(s: str) -> bool:
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
-
-    pass
+    stack = Stack(len(s), dtype=str)
+    open = "([{"
+    close = ")]}"
+    for i in s:
+        if i in open:
+            stack.push(i)
+        elif i in close:
+            previous = get_starting_symbol(i)
+            if stack.empty() or stack.pop() != previous:
+                return False
+    return stack.empty()
 
 
 if __name__ == "__main__":
     # Let's solve Valid Parentheses problem from leetcode.com:
     # https://leetcode.com/problems/valid-parentheses/
     cases = []
-    with open("practicum_3/homework/basic/valid_parentheses_cases.yaml", "r") as f:
+    with open("C://Users//Alexandra//Documents//Универ//Algos//spbu-fundamentals-of-algorithms//practicum_3//homework//basic//valid_parentheses_cases.yaml", "r") as f:
         cases = yaml.safe_load(f)
     for c in cases:
         res = are_parentheses_valid(c["input"])
